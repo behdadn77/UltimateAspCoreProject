1c2a94117d87e9933d6dd9e21f3a670a416050c2
Behdad Nemati <behdad.nemati1@gmail.com>
2020-08-18 10:38:08 +0430

    added res status and personal attr

---
+++ Common/Messages/ResualtStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Messages
+{
+    class ResualtStatus
+    {
+        public class ResultStatus
+        {
+            public ResultStatus()
+            {
+
+            }
+
+            public ResultStatus(string title, string message, MessageType type, bool isSuccessed, Exception errorException)
+            {
+                Title = title;
+                Message = message;
+                Type = type;
+                IsSuccessed = isSuccessed;
+                ErrorException = errorException;
+            }
+            public string Title { get; set; }
+            public string Message { get; set; }
+            public MessageType Type { get; set; }
+            public bool IsSuccessed { get; set; }
+            public Exception ErrorException { get; set; }
+        }
+
+    }
+}
