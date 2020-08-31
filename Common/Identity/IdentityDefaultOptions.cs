970cebcd51f2cf6ff6ff5c6e7c07062f71c95199
Behdad Nemati <behdad.nemati1@gmail.com>
2020-08-31 12:18:14 +0430

    PasswordRequirements

--- Common/Identity/IdentityDefaultOptions.cs
+++ Common/Identity/IdentityDefaultOptions.cs
@@ -14,4 +14,15 @@ namespace Common.Identity
         public string EmailAddress { get; set; }
         public string Password { get; set; }
     }
+    public class PasswordRequirements
+    {
+        public string Regex { get; set; }
+        public int MinLenght { get; set; }
+        public int MaxLenght { get; set; }
+        public bool AllowWhiteSpace { get; set; }
+        public int RequiredDigits { get; set; }
+        public int RequiredUppercase { get; set; }
+        public int RequiredLower { get; set; }
+        public int RequiredSpecialChar { get; set; }
+    }
 }
